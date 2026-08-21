4831f55804e4ba6299a9ea3039c0234ff9039ac0
_Pragya__Gupta_ <pragyagupta5467@gmail.com>
2026-08-22 02:27:17 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -176,7 +176,7 @@ function App() {
     setShowResults(false);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://diabetes-prediction-system-1-qw0.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
